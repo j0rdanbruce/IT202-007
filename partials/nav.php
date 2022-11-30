ae40cc5e78981bc2db7126c31943430eee4ada42
j0rdanbruce <jeb79@njit.edu>
2022-11-30 14:57:46 -0500

    made css styles to nav.php

--- partials/nav.php
+++ partials/nav.php
@@ -29,6 +29,34 @@ if (($localWorks && $domain == "localhost") || $domain != "localhost") {
 
 require(__DIR__ . "/../lib/functions.php");
 ?>
+
+<style>
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: teal;
+    }
+
+    li {
+        float: left;
+    }
+
+    li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+
+    /* Change the link color to #111 (black) on hover */
+    li a:hover {
+        background-color: red;
+    }
+</style>
+
 <nav>
     <ul>
         <?php        
