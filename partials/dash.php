30d412de5890e7271c95f02e18e673a6ebb2bf17
j0rdanbruce <jeb79@njit.edu>
2022-12-12 12:33:44 -0500

    the initial button layout for banking system

--- partials/dash.php
+++ partials/dash.php
@@ -28,52 +28,7 @@ require_once(__DIR__ . "/../lib/functions.php");
 ?>
 
 <style>
-    ul {
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
-        overflow: hidden;
-        background-color: teal;
-        border-radius:10px;
-    }
-
-    li {
-        float: left;
-    }
-
-    li a {
-        display: block;
-        color: white;
-        text-align: center;
-        padding: 14px 16px;
-        text-decoration: none;
-    }
-
-    /* Change the link color to #111 (black) on hover */
-    li a:hover {
-        background-color: red;
-    }
-
-    .center-screen li {
-        float: none;
-    }
-
-    .center-screen ul {
-        background-color: grey;
-    }
-    .center-screen {
-        border-style: inset;
-        border-color: teal;
-        border-width: 5px;
-
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        text-align: center;
-        min-height: 60vh;
-    }
-
-
+    
 </style>
 
 <!-- include css and js files -->
