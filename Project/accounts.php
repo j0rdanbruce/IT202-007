027e896bdadd7302b0b029bc7fbe411d7a1c7b1c
j0rdanbruce <jeb79@njit.edu>
2022-12-15 16:27:17 -0500

    created display table of first 4 account for specific user

--- Project/accounts.php
+++ Project/accounts.php
@@ -5,29 +5,59 @@ is_logged_in(true);
 $username = get_username();
 ?>
 
-<h1>$username's Checking Account(s):</h1>
+<script>
+    function getAccount(accountNum){
+        return accountNum;
+    }
+</script>
 
-<form onsubmit="return validate(this)" method="POST">
-    <div>
-        <label for="deposit">Deposit Amount:</label>
-        <input type="number" name="deposit" required placeholder="e.g. 10"/>
-    </div>
-    <input type="submit" value="Submit" />
-</form>
+<h1>{$username}'s Checking Account(s):</h1>
 
-<?php
-    if (isset($_POST["deposit"])){
-        $deposit = se($_POST, "deposit", "", false);
-        $hasError = false;
+<html>
+    <body>
+    <?php
+        //access database
+        $db = getDB();
+        $stmt = $db->prepare("SELECT accountNum, accountType, modified, balance FROM Account WHERE userID = :userID LIMIT 4 OFFSET 0");
+        $stmt->execute([":userID"=>get_user_id()]);
+        //$result = $stmt->fetch(PDO::FETCH_ASSOC);
+        //echo get_user_id();
+        echo "<table border='1'>
+            <tr>
+                <th>Account Number</th>
+                <th>Account Type</th>
+                <th>Modified Date</th>
+                <th>Current Balance</th>
+            </tr>";
+            while ($result = $stmt->fetch(PDO::FETCH_ASSOC)){
+                $accountNum = $result["accountNum"];
+                $accountType = $result["accountType"];
+                $modified = $result["modified"];
+                $balance = $result["balance"];
+                echo "<tr>";
+                    echo "<td>";
+                        echo "<form method='POST'>";
+                            echo "<input type='button' value='$accountNum' name='accountNum'/>";
+                        echo "</form>";
+                    echo "</td>";
+                    //echo "<td> <a href='account.php' onclick='getAccount($accountNum)'>" . $accountNum . "</td>";
+                    echo "<td>" . $accountType . "</td>";
+                    echo "<td>" . $modified . "</td>";
+                    echo "<td>" . $balance . "</td>";
+                echo "</tr>";
 
-        if (!$hasError){
+            }
+        echo "</table>";
 
+        //<form onsubmit="return validate(this)" method="POST">
+        if (isset($_POST["accountNum"])){
+            $accountNum = se($_POST, "accountNum", "", false);
+            echo $accountNum;
         }
 
-    }
-
-?>
-
+    ?>
+    </body>
+</html>
 
 
 
