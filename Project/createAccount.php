ab2a5983e943296cc7fe5c09bdd1c0371219071a
j0rdanbruce <jeb79@njit.edu>
2022-12-15 12:56:53 -0500

    made final changes to Account and Transactions tables and updated transaction protocol

--- Project/createAccount.php
+++ Project/createAccount.php
@@ -41,40 +41,61 @@ is_logged_in(true);
             flash("Initial deposit must be at least $10", "warning");
             $hasError = true;
         }
-
+        //insert checking account into Account table and grab checking account id
         if (!$hasError){
             $stmt = $db->prepare("INSERT INTO Account (accountNum, userID, balance, accountType) VALUES(:accountNum, :userID, :balance, :accountType)");
+            $stmt2 = $db->prepare("SELECT id FROM Account WHERE accountNum = :accountNum");
             try{
                 $stmt->execute([":accountNum" => $accntNum, ":userID" => get_user_id(), ":balance" => $deposit, ":accountType" => "checking"]);
+                $stmt2->execute([":accountNum"=>$accntNum]);
+                $result = $stmt2->fetch(PDO::FETCH_OBJ);
+                $accountId = $result->id;
             }catch (Exception $e) {
                 users_check_duplicate($e->errorInfo);
             }
         }
-
         //get current balance of world
         if (!$hasError){
             $stmt = $db->prepare("SELECT balance FROM Account WHERE userID = :userID");
             try{
                 $stmt->execute([":userID" => -1]);
                 $result = $stmt->fetch(PDO::FETCH_OBJ);
-                $currentBalance = $result->balance;
-                var_dump((int)$currentBalance);
+                $currentBalance = (int)$result->balance;
+                //var_dump((int)$currentBalance);
+            }catch(Exception $e){
+                users_check_duplicate($e->errorInfo);
+            }
+            //update balance of world
+            $updateBalance = $currentBalance - $deposit;
+            $stmt = $db->prepare("UPDATE Account SET balance = :balance WHERE userID = :userID");
+            try{
+                $stmt->execute([":balance"=>$updateBalance, ":userID"=>-1]);
             }catch(Exception $e){
                 users_check_duplicate($e->errorInfo);
             }
         }
-        
-        /*if (!$hasError){
-            $stmt = $db->prepare("INSERT INTO Transactions (accountSrc, accountDest, balanceChg, transType, memo, expectedTotal) 
-                                    VALUES (:accountSrc, :accountDest, :balanceChg, transType, :memo, :expectedTotal)");
+        //make transaction pair here
+        if (!$hasError){
+            $stmt1 = $db->prepare("INSERT INTO Transactions (accountSrc, accountDest, balanceChg, transType, memo, expectedTotal) 
+                                    VALUES (:accountSrc, :accountDest, :balanceChg, :transType, :memo, :expectedTotal)");
             try{
-                $stmt->execute([":accountSrc" => -1, ":accountDest" => get_user_id(), ":balanceChg" => (-1 * $deposit), 
-                                    ":transType" => "create", ":memo" => "", ":expectedTotal" => (-1 * $deposit)]);
+                $stmt1->execute([":accountSrc" => -1, ":accountDest" => get_user_id(), ":balanceChg" => (-1 * $deposit), 
+                                    ":transType" => "create", ":memo" => "", ":expectedTotal" => ($updateBalance)]);
             }catch(Exception $e) {
                 users_check_duplicate($e->errorInfo);
             }
-        }*/
-        flash("Successfully Registered for Checking Account! '\n'Funds have been added to your checking account.", "success");
+        }
+        if (!$hasError){
+            $stmt2 = $db->prepare("INSERT INTO Transactions (accountSrc, accountDest, balanceChg, transType, memo, expectedTotal) 
+                                    VALUES (:accountSrc, :accountDest, :balanceChg, :transType, :memo, :expectedTotal)");
+            try{
+                $stmt2->execute([":accountSrc" => get_user_id(), ":accountDest" => -1, ":balanceChg" => ($deposit), 
+                                    ":transType" => "create", ":memo" => "create account " . $accountId, ":expectedTotal" => ($deposit)]);
+            }catch(Exception $e){
+                users_check_duplicate($e->errorInfo);
+            }
+        }
+        flash("Successfully Registered for Checking Account! '\nFunds have been added to your checking account.", "success");
         //die(header("Location: accounts.php"));
     }
 ?>
