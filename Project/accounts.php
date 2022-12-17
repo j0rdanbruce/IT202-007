035f822802e40b42194ee37e4de7f41aa86ad3a5
j0rdanbruce <jeb79@njit.edu>
2022-12-17 15:57:34 -0500

    updated acounts feature to show data of selected account number

--- Project/accounts.php
+++ Project/accounts.php
@@ -11,53 +11,108 @@ $username = get_username();
     }
 </script>
 
-<h1>{$username}'s Checking Account(s):</h1>
+<h1><?php echo get_username(); ?>'s Checking Account(s):</h1>
 
-<html>
-    <body>
-    <?php
-        //access database
-        $db = getDB();
-        $stmt = $db->prepare("SELECT accountNum, accountType, modified, balance FROM Account WHERE userID = :userID LIMIT 4 OFFSET 0");
-        $stmt->execute([":userID"=>get_user_id()]);
-        //$result = $stmt->fetch(PDO::FETCH_ASSOC);
-        //echo get_user_id();
-        echo "<table border='1'>
-            <tr>
-                <th>Account Number</th>
-                <th>Account Type</th>
-                <th>Modified Date</th>
-                <th>Current Balance</th>
-            </tr>";
+<body>
+    <table border="1">
+        <tr>
+            <th>Account Number</th>
+            <th>Account Type</th>
+            <th>Modified Date</th>
+            <th>Current Balance</th>
+        </tr>
+        <?php
+            $db = getDB();
+            $stmt = $db->prepare("SELECT accountNum, accountType, modified, balance FROM Account WHERE userID = :userID LIMIT 4 OFFSET 0");
+            $stmt->execute([":userID"=>get_user_id()]);
             while ($result = $stmt->fetch(PDO::FETCH_ASSOC)){
                 $accountNum = $result["accountNum"];
                 $accountType = $result["accountType"];
                 $modified = $result["modified"];
-                $balance = $result["balance"];
-                echo "<tr>";
-                    echo "<td>";
-                        echo "<form method='POST'>";
-                            echo "<input type='button' value='$accountNum' name='accountNum'/>";
-                        echo "</form>";
-                    echo "</td>";
-                    //echo "<td> <a href='account.php' onclick='getAccount($accountNum)'>" . $accountNum . "</td>";
-                    echo "<td>" . $accountType . "</td>";
-                    echo "<td>" . $modified . "</td>";
-                    echo "<td>" . $balance . "</td>";
-                echo "</tr>";
-
-            }
-        echo "</table>";
-
-        //<form onsubmit="return validate(this)" method="POST">
+                $balance = $result["balance"];  
+            
+        ?>
+        <tr>
+            <td>
+                <form onsubmit="return validate(this)" method="POST">
+                    <input type="submit" value="<?php echo $accountNum; ?>" name="accountNum" >
+                </form>
+            </td>
+            <td><?php echo $accountType; ?></td>
+            <td><?php echo $modified; ?></td>
+            <td><?php echo $balance; ?></td>
+        </tr>
+        <?php }; ?>
+    </table>
+<br><br><br>
+<h2>
+    <?php
         if (isset($_POST["accountNum"])){
             $accountNum = se($_POST, "accountNum", "", false);
-            echo $accountNum;
+            echo "Transaction History for Account Number: " . $accountNum;
         }
-
     ?>
-    </body>
-</html>
+</h2>
+
+    <table border="1">
+        <tr>
+            <th>Account Number</th>
+            <th>Account Type</th>
+            <th>Balance</th>
+            <th>Created On</th>
+        </tr>
+        <?php
+            if (isset($_POST["accountNum"])){
+                $accountNum = se($_POST, "accountNum", "", false);
+            }
+            $db = getDB();
+            $stmt = $db->prepare("SELECT accountType, balance, created FROM Account WHERE accountNum = :accountNum");
+            try{
+                $stmt->execute([":accountNum"=>$accountNum]);
+                $result = $stmt->fetch(PDO::FETCH_OBJ);
+                $accountType = $result->accountType;
+                $balance = $result->balance;
+                $created = $result->created;
+        ?>
+        <tr>
+            <td><?php echo $accountNum; ?></td>
+            <td><?php echo $accountType; ?></td>
+            <td><?php echo $balance; ?></td>
+            <td><?php echo $created; ?></td>
+        </tr>
+        <?php
+            }catch(Exception $e){
+                users_check_duplicate($e->errorInfo);
+            }
+        ?>
+    </table>
+
+    <div>
+        <?php 
+            if (isset($_POST["accountNum"])){
+                $accountNum = se($_POST, "accountNum", "", false);
+            }
+            $db = getDB();
+            //$stmt1 = $db->prepare("SELECT id FROM Account WHERE accountNum = :accountNum");
+            $stmt2 = $db->prepare("SELECT Account.id, accountNum, accountSrc, accountDest, transType, balanceChg, created, expectedTotal, memo 
+                                FROM Transactions INNER JOIN Account ON Transactions.
+                                WHERE accountSrc = :accountSrc OR accountNum = :accountNum2 LIMIT 12 OFFSET 0");
+            try{
+                //$result = $stmt1->execute([":accountNum"=>$accountNum]);
+                //$id = $result->id;
+                $result = $stmt2->execute();
+            }catch(Exception $e){
+                users_check_duplicate($e->errorInfo);
+            }
+        ?>
+        <table>
+
+        </table>
+    </div>
+
+
+
+</body>
 
 
 
